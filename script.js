4e6d61218973227aae261107b51f83ac6aa8c6ea
kari-osk <karinaosuka@gmail.com>
2021-11-08 21:09:22 -0300

    inclusão do input checkbox

--- script.js
+++ script.js
@@ -1,55 +1,40 @@
-let fieldTitle = document.querySelector('#fieldTitle');
-let fieldDescription = document.querySelector('#fieldDescription');
-let fieldUrl = document.querySelector('#fieldUrl');
-let btn = document.querySelector('#btn');
+let fieldTitle = document.querySelector('#fieldTitle')
+let fieldDescription = document.querySelector('#fieldDescription')
+let fieldUrl = document.querySelector('#fieldUrl')
+let btn = document.querySelector('#btn')
 
-let remove = document.querySelector('#remove');
+let remove = document.querySelector('#remove')
 
-let list = document.querySelector('ol');
+let list = document.querySelector('ol')
 
-btn.addEventListener('click', insertIn);
+btn.addEventListener('click', insertIn)
 
-function insertIn(event){
-  event.preventDefault();
+function insertIn(event) {
+  event.preventDefault()
 
-  let tipoTur = document.querySelector('[name="tipoTurismo"]:checked').value;
+  let tipoTur = document.querySelector('[name="tipoTurismo"]:checked').value
 
-  let nacInter = document.querySelector('[name="nacInter"]:checked').value;
+  let nacInter = document.querySelector('[name="nacInter"]:checked').value
 
-  let li = document.createElement('li');
-  li.innerText = `Tipo de turismo: ${tipoTur};\n Viagem: ${nacInter}; \n Destino: ${fieldTitle.value};\n Descrição: ${fieldDescription.value}`;
-  document.querySelector('ol').appendChild(li);
+  let passaporte = document.querySelector('#passaporte')
+  passaporte.checked
+    ? (passaporteTxt = 'Precisa de passaporte')
+    : (passaporteTxt = 'Não precisa de passaporte')
 
-  document.querySelector('#btn').onclick = function(){
-    let radios = document.getElementsByName('genero');
-    for(let i = 0; i < radios.clientHeight; i++){
-      if (radios[i].checked){
-        document.querySelector('ol').appendChild(radios);
+  let li = document.createElement('li')
+  li.innerText = `Tipo de turismo: ${tipoTur};\n Tipo de viagem: ${nacInter};\n ${passaporteTxt};\n Destino: ${fieldTitle.value}; \n Descrição: ${fieldDescription.value}.`
+  document.querySelector('ol').appendChild(li)
+
+  document.querySelector('#btn').onclick = function () {
+    let radios = document.getElementsByName('genero')
+    for (let i = 0; i < radios.clientHeight; i++) {
+      if (radios[i].checked) {
+        document.querySelector('ol').appendChild(radios)
       }
     }
   }
-  
-  let img = document.createElement('img');
-  img.setAttribute('src', fieldUrl.value);
-  document.querySelector('ol').appendChild(img);
-};
 
-remove.addEventListener('click', removeAll);
-function removeAll(event){
-  event.preventDefault();
-
+  let img = document.createElement('img')
+  img.setAttribute('src', fieldUrl.value)
+  document.querySelector('ol').appendChild(img)
 }
-
-
-
-
-
-
-
-// let remove = document.querySelector('#remove');
-// remove.addEventListener('click', remove);
-// function remove(event){
-//   event.preventDefault();
-//   remove.appendChild();
-// };
-
